51f1c91a722c61ffd4aa72c1d04fc918aded46d0
seedoffd <seedoffd@gmail.com>
2020-02-08 10:34:49 -0600

    groovyfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,1 +1,14 @@
+node {
+    stage("Pull Repo"){
 
+    }
+    stage('Build Image'){
+        sh "parker version"    
+    }
+    stage("send notification to slack"){
+        echo "Hello"
+        
+    }
+    stage("send email"){
+        echo "Hello"
+}
