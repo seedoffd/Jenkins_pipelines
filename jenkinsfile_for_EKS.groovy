13b3a61de7081fc4650fd91819d312d6ff249dd4
seedoffd <seedoffd@gmail.com>
2020-02-08 12:45:08 -0600

    Jenkins EKS

---
+++ jenkinsfile_for_EKS.groovy
@@ -0,0 +1,18 @@
+node{
+    stage("Pull Repo"){
+        git "https://github.com/bkarakas/terraform-iaac-eks-burak.git"
+
+    }
+    stage("stage1"){
+        echo"Hello"
+        
+    }
+    stage("stage1"){
+        echo"Hello"
+        
+    }
+    stage("stage1"){
+        echo"Hello"
+        
+    }
+}
