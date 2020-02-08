24c4ffe07ce88cf15649cc8e3b6b33d624f64651
seedoffd <seedoffd@gmail.com>
2020-02-08 13:22:50 -0600

    terraform version updated

--- jenkinsfile_for_EKS.groovy
+++ jenkinsfile_for_EKS.groovy
@@ -1,18 +1,18 @@
 node{
     stage("Pull Repo"){
-        git "https://github.com/bkarakas/terraform-iaac-eks-burak.git"
+        git 'https://github.com/seedoffd/terraform-iaac-eks-burak.git'
+    }
+    stage("Download Terraform"){
+        sh "terraform version"
+        sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+        sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+        sh "./terraform version"
+    }
+    stage("Set Backend"){
+        sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
+    }
+    stage("stage1"){
+        echo "Hello"
 
-    }
-    stage("stage1"){
-        echo"Hello"
-        
-    }
-    stage("stage1"){
-        echo"Hello"
-        
-    }
-    stage("stage1"){
-        echo"Hello"
-        
     }
 }
